adb086aaf185d83f480d3beccbaaa7477c85716e
Nicholas Sullivan <sullnich@sheridancollege.ca>
2025-11-19 13:53:07 -0500

    Initial Commit

---
+++ MobileApp/ShockTrackMobileApp/ShockTrackMobileApp/ShockTrackMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShockTrackMobileAppApp.swift
+//  ShockTrackMobileApp
+//
+//  Created by Nicholas Sullivan on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct ShockTrackMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
